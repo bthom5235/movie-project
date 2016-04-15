e01b6bd0ee497be8d43e353a16a272f35e72d514
bthom5235 <tifaheart68@gmail.com>
2016-04-15 14:18:48 +0000

    movie-project

--- solution.js
+++ solution.js
@@ -1,7 +1,5 @@
-/****************************************************
-1. Create an array called movieQueue with at least two Movie Names as strings
-****************************************************/
 
+var movieQueue = ["NoteBook", "Titanic"];
 
 
 /****************************************************
@@ -10,7 +8,10 @@
 - It should add the new movie name to the end of your movieQueue array
 - Finally it should return the movie name it just added
 ****************************************************/
-
+function addMovieToEnd (allMovie) {
+  movieQueue.push(allMovie);
+  return allMovie; 
+}
 
 
 /****************************************************
@@ -19,7 +20,10 @@
 - It should add the new movie name to the front of your movieQueue array
 - Finally it should return the movie name it just added
 ****************************************************/
-
+function addMovieToFront (allMovie){
+  movieQueue.unshift(allMovie);
+  return allMovie;
+}
 
 
 /****************************************************
@@ -29,8 +33,10 @@
 - It should add the new movie name to the movieQueue array at the given index.
 - Finally it should return the movie name it just added
 ****************************************************/
-
-
+function changeMovie (location, newmovie ) {
+  movieQueue [location] = newmovie;
+  return newmovie;
+}
 
 /****************************************************
 5. Create a function called getMovie
@@ -40,5 +46,11 @@
 - If the index is not valid (meaning that it's larger than the number of items in the array),
   it should return the string "not a valid index"
 ****************************************************/
-
-
+function getMovie (index) {
+  if (index < movieQueue.length) {
+    return movieQueue [index];
+    }
+    else {
+      return ("not a valid index");
+    }
+}
